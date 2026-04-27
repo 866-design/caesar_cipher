2bbeb3250e93ee52a43fe45c6298b8f97af1fa2c
Sundiata Dumisani <sdumi866@gmail.com>
2026-04-27 23:50:08 +0000

    Initial Caesar cipher implementation

--- caesar.py
+++ caesar.py
@@ -8,3 +8,20 @@ def caesar_cipher(message, shift):
       shifted = chr((ord(upper) - ord('A') + shift) % 26 + ord('A'))
       encoded.append(shifted)
   return encoded
+
+def main():
+  if len(sys.argv) < 2:
+    print("Usage: python caesar.py <shift>")
+    sys.exit(1)
+
+  shift = int(sys.argv[1])
+  encoded_chars = []
+
+  for line in sys.stdin:
+    encoded_chars.extend(caesar_cipher(line, shift))
+
+  block_size = 5
+  blocks_per_line = 10
+  
+  blocks = []
+  for i in range
